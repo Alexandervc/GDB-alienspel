4e2344d8838e6f93374c80b17d909bd929bbaf72
Stan <stan.vandenbroek@student.fontys.nl>
2015-04-01 18:44:41 +0200

    2 player movement toegevoegd gebaseert op het bewegen van de voeten

---
+++ Unity/Shader testing/Assets/scripts/Movement/FootDriver.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootDriver : MonoBehaviour 
+{
+    public bool WASD = true;
+
+    public float Speed = 0.5f;
+
+    public float MaxDistance;
+    public float ExtendedDistance;
+
+    public Transform AttachmentPoint;
+
+    public bool Grounded = false;
+
+    public bool Extending = false;
+
+    public float CurrentDistance
+    {
+        get
+        {
+            return (this.transform.position - AttachmentPoint.position).magnitude;
+        }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if(WASD)
+        {
+            if (Input.GetKey(KeyCode.W)) direction += new Vector3(0, 1, 0);
+            if (Input.GetKey(KeyCode.A)) direction += new Vector3(-1, 0, 0);
+            if (Input.GetKey(KeyCode.S)) direction += new Vector3(0, -1, 0);
+            if (Input.GetKey(KeyCode.D)) direction += new Vector3(1, 0, 0);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.I)) direction += new Vector3(0, 1, 0);
+            if (Input.GetKey(KeyCode.J)) direction += new Vector3(-1, 0, 0);
+            if (Input.GetKey(KeyCode.K)) direction += new Vector3(0, -1, 0);
+            if (Input.GetKey(KeyCode.L)) direction += new Vector3(1, 0, 0);
+        }
+
+        direction *= this.Speed * Time.deltaTime;
+
+        Vector3 distance = this.transform.position - AttachmentPoint.position;
+
+        this.Extending = false;
+        if (distance.magnitude < this.ExtendedDistance)
+        {
+            Vector3 projDir = Vector3.Project(direction, distance);
+
+            if (projDir.normalized == distance.normalized)
+            {
+                this.Extending = true;
+            }
+        }
+
+        if (this.Grounded && direction.y < 0) direction.y = 0;
+
+        this.transform.position += direction;
+
+        if(distance.magnitude > this.MaxDistance)
+        {
+            this.transform.position = AttachmentPoint.position + distance.normalized * this.ExtendedDistance;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            this.Grounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            this.Grounded = false;
+        }
+    }
+}
---
+++ Unity/Shader testing/Assets/scripts/Movement/Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+    public FootDriver Foot1;
+    public FootDriver Foot2;
+
+    public float Speed = 5.0f;
+    public float MinXDistance = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Foot1.Grounded && !Foot2.Grounded)
+        {
+            this.MoveTowardsFoot(Foot2, Foot1);
+        }
+        else if (!Foot1.Grounded && Foot2.Grounded)
+        {
+            this.MoveTowardsFoot(Foot1, Foot2);
+        }
+        else if(!Foot1.Grounded && !Foot2.Grounded)
+        {
+            Vector3 movement = new Vector3(0, -1, 0) * this.Speed * Time.deltaTime;
+            this.transform.position += movement;
+        }
+
+        if((Foot1.Grounded && Foot1.Extending) ||
+            (Foot2.Grounded && Foot2.Extending))
+        {
+            Vector3 movement = new Vector3(0, 1, 0) * this.Speed * Time.deltaTime;
+            this.transform.position += movement;
+
+            Foot1.transform.position -= movement;
+            Foot2.transform.position -= movement;
+        }
+	}
+
+    private void MoveTowardsFoot(FootDriver lifted, FootDriver grounded)
+    {
+        if(lifted.CurrentDistance > lifted.ExtendedDistance)
+        {
+            Vector3 difference = lifted.transform.position - this.transform.position;
+
+            difference.y = 0;
+            difference.z = 0;
+
+            if(difference.x > this.MinXDistance)
+            {
+                Vector3 movement = difference.normalized * this.Speed * Time.deltaTime;
+
+                this.transform.position += movement;
+
+                Foot1.transform.position -= movement;
+                Foot2.transform.position -= movement;
+            }
+        }
+    }
+}
